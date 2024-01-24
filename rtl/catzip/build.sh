ac6a6c7fc1c9a51124722cefa233cd4312b9ea2e
Ed Vidal Jr <develone@sbcglobal.net>
2024-01-24 16:20:55 -0700

    now creates a top.bin that passes Info: Program finished normally. // Reading input .asc file.. // Reading 8k chipdb file.. // Creating timing netlist.. // Timing estimate: 8.77 ns (114.04 MHz) // Checking 10.00 ns (100.00 MHz) clock constraint: PASSED.

--- rtl/catzip/build.sh
+++ rtl/catzip/build.sh
@@ -1,6 +1,8 @@
 !/bin/bash
 rm -f uart.json uart.asc uart.bin *.log
-yosys -l yosys-uart.log -p "read_verilog uart.v; synth_ice40 -top uart -json uart.json"
-nextpnr-ice40 --hx8k --pcf-allow-unconstrained --package ct256 -l ../place_and_route.log --pcf helloworld.pcf --json uart.json --asc uart.asc
-icepack uart.asc uart.bin
-icetime -d hx8k -c 100 uart.asc
+export PIN_DEF = $(CURDIR)/constraint/helloworld.pcf
+export DEVICE = hx8k
+yosys -l yosys-uart.log -p "read_verilog top.v reset.v uart.v ; synth_ice40 -top top -json top.json"
+nextpnr-ice40 --hx8k --pcf-allow-unconstrained --package ct256 -l ../place_and_route.log --pcf helloworld.pcf --json top.json --asc top.asc
+icepack top.asc top.bin
+icetime -d hx8k -c 100 top.asc
